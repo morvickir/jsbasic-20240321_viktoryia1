2ecab123f5c4eba9aeb429cc9c5ae306ac114df7
Victoria Morozova <morozova.vic@office.lan>
2024-05-13 11:49:35 +0300

    5-module-3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
-}
+    const carouselInner = document.querySelector('.carousel__inner');
+    const carouselArrows = document.querySelectorAll('.carousel__arrow');
+    const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+    const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+    const slideWidth = carouselInner.offsetWidth;
+    let currentSlide = 0;
+  
+    function moveSlide(direction) {
+      if (direction === 'left') {
+        currentSlide--;
+      } else {
+        currentSlide++;
+      }
+  
+      carouselInner.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+      if (currentSlide === 0) {
+        carouselArrowLeft.style.display = 'none';
+        carouselArrowRight.style.display = '';
+      } else if (currentSlide === 3) {
+        carouselArrowLeft.style.display = '';
+        carouselArrowRight.style.display = 'none';
+      } else {
+        carouselArrows.forEach(arrow => arrow.style.display = '');
+      }
+    }
+  
+    carouselArrowLeft.addEventListener('click', () => moveSlide('left'));
+    carouselArrowRight.addEventListener('click', () => moveSlide('right'));
+  
+    carouselArrowLeft.style.display = 'none';
+  }
+  
