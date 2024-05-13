370895db71019587156934dadad89414a5643a2f
Victoria Morozova <morozova.vic@office.lan>
2024-05-13 11:43:36 +0300

    5-module-2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  const textDiv = document.getElementById('text');
+  const button = document.querySelector('.toggle-text-button');
+  
+  button.addEventListener('click', () => {
+    if (textDiv.hidden) {
+      textDiv.hidden = false;
+    } else {
+      textDiv.hidden = true;
+    }
+  });
 }
